60e23342645389605c72332e07c96e11f91d394d
SimaDovakin <simadovakin999@gmail.com>
2022-08-19 12:45:57 +0300

    Fixed bug with unselection event.

--- vocabulary/actions/listbox_actions.py
+++ vocabulary/actions/listbox_actions.py
@@ -4,19 +4,20 @@ import tkinter as tk
 def onselect(event, text_box):
     word_list = event.widget
 
-    selection_index = int(word_list.curselection()[0])
-    word = word_list.get(selection_index)
+    if word_list.curselection():
+        selection_index = int(word_list.curselection()[0])
+        word = word_list.get(selection_index)
 
-    translation = next(
-        filter(
-            lambda item: item['word'] == word,
-            word_list.word_list
-        )
-    ).get('translation')
+        translation = next(
+            filter(
+                lambda item: item['word'] == word,
+                word_list.word_list
+            )
+        ).get('translation')
 
-    text_box.delete('1.0', tk.END)
+        text_box.delete('1.0', tk.END)
 
-    if translation:
-        text_box.insert(tk.END, translation)
-    else:
-        text_box.insert(tk.END, 'Content not found.')
+        if translation:
+            text_box.insert(tk.END, translation)
+        else:
+            text_box.insert(tk.END, 'Content not found.')
