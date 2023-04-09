0ae753e98474ad713cd0c32dece446ae7fae70e3
SimaDovakin <simadovakin999@gmail.com>
2023-04-09 13:55:35 +0300

    Implemented actions for the word deletion, word description selection and word description edition.

--- vocabulary/actions/listbox_actions.py
+++ vocabulary/actions/listbox_actions.py
@@ -1,6 +1,33 @@
 import tkinter as tk
 
+from vocabulary.db.deletes import delete_word
+from vocabulary.db.selects import get_word_description
+
 
 def onselect(event, text_box):
-    print("On select")
-    # TODO: implement on select logic
+    list_box = event.widget
+
+    selected_word_indexes = list_box.curselection()
+    if not selected_word_indexes:
+        return
+
+    word = list_box.get(selected_word_indexes[0])
+    word_description = get_word_description(word)
+    word_description = word_description or ""
+
+    text_box.delete('1.0', tk.END)
+    text_box.insert(tk.END, word_description)
+
+
+def onpressdelete(event):
+    list_box = event.widget
+
+    selected_word_indexes = list_box.curselection()
+    if not selected_word_indexes:
+        return
+
+    selected_word = list_box.get(selected_word_indexes[0])
+
+    delete_word(selected_word)
+
+    list_box.delete(selected_word_indexes[0])
--- vocabulary/actions/textbox_actions.py
+++ vocabulary/actions/textbox_actions.py
@@ -1,6 +1,17 @@
 import tkinter as tk
 
+from vocabulary.db.updates import update_word_description
 
-def oninput(event):
-    print("On input")
-    # TODO: implement on input logic
+
+def oninput(event, list_box):
+    text_box = event.widget
+
+    selected_word_indexes = list_box.curselection()
+    if not selected_word_indexes:
+        return
+
+    word = list_box.get(selected_word_indexes[0])
+    description = text_box.get("1.0", tk.END)
+    description = description or ""
+
+    update_word_description(word, description)
